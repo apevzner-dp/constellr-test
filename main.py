01fa0965403796a6654151ac30153108d66292cf
Alexander Pevzner <a.pevzner.dp@gmail.com>
2023-01-27 10:24:25 +0100

    Initial commit

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ if __name__ == '__main__':
     calculator = EVICalculator(
         polygon_file='aoi.geojson',
         n_days_ago=5*365,
-        output_file='output.zarr'
+        zarr_output_dir='output.zarr'
     )
     calculator.calculate()
